b8870bf214f73729459ca8dd71b1ad4820fb6ba6
Danilo Pimentel <danilo.pimentel97@gmail.com>
2015-10-01 10:15:39 -0300

    Iniciando
    Configurações do db alteradas

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 
 class Connection
 {
-  public static $database = "serasa";
-  public static $address = "127.0.0.1";
-  public static $user = "root";
-  public static $password = "";
+  public static $database = "daw-aluno11";
+  public static $address = "150.164.102.160";
+  public static $user = "daw-aluno11";
+  public static $password = "danilo";
 
   public static function getConnection() {
     $connection = mysqli_connect(Connection::$address, Connection::$user, Connection::$password, Connection::$database);
